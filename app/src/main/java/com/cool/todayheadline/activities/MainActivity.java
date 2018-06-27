a0ea2fa48aea5be7b4ba9ecab4534880d5c50f42
FoxyWinner <610023448@qq.com>
2018-06-27 11:16:56 +0800

    主框架配置完成，可以开始写fragment了。

---
+++ app/src/main/java/com/cool/todayheadline/activities/MainActivity.java
@@ -0,0 +1,79 @@
+package com.cool.todayheadline.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.cool.todayheadline.R;
+import com.cool.todayheadline.extend.MainActivityViewPagerAdapter;
+import com.cool.todayheadline.extend.NoSlidingViewPaper;
+
+public class MainActivity extends AppCompatActivity
+{
+
+
+    private NoSlidingViewPaper mpager;
+    private MainActivityViewPagerAdapter mainActivityViewPagerAdapter;
+    private BottomNavigationView mNavigation;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener()
+    {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item)
+        {
+            switch (item.getItemId())
+            {
+                case R.id.navigation_home:
+                    mpager.setCurrentItem(0);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mpager.setCurrentItem(1);
+                    return true;
+                case R.id.navigation_notifications:
+                    mpager.setCurrentItem(2);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findView();
+
+        init();
+
+    }
+
+    public void findView()
+    {
+        mNavigation = (BottomNavigationView) findViewById(R.id.navigation);
+//        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        mpager = (NoSlidingViewPaper) findViewById(R.id.vp_main_container);
+    }
+
+    public void init()
+    {
+        //为底部导航栏添加监听事件
+        mNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mainActivityViewPagerAdapter = new MainActivityViewPagerAdapter(getSupportFragmentManager());
+
+//        为Adapter添加Fragment
+//        mainActivityViewPagerAdapter.addFragment(new HomeFragment());
+//        mainActivityViewPagerAdapter.addFragment(new FavoriteFragment());
+//        mainActivityViewPagerAdapter.addFragment(new SettingsFragment());
+        mpager.setAdapter(mainActivityViewPagerAdapter);
+
+    }
+
+}
---
+++ app/src/main/java/com/cool/todayheadline/extend/MainActivityViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.cool.todayheadline.extend;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xiaon on 2017/7/13.
+ */
+
+public class MainActivityViewPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments = new ArrayList<>();
+
+    public MainActivityViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        fragments.add(fragment);
+    }
+
+}
