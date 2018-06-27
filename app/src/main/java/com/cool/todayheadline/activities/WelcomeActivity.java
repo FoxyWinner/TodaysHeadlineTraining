901092ba4dde23b4812f8b9c903a3ba22e3dcee1
kansas <lilihehe2007@163.com>
2018-06-27 12:55:33 +0800

    增添了欢迎页面

---
+++ app/src/main/java/com/cool/todayheadline/activities/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.cool.todayheadline.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cool.todayheadline.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
